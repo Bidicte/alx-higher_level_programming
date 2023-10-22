d87b04c5225d089f307d5317a0cb69bddf0e72ab
Bidicte <malexandra13@yahoo.com>
2023-10-22 16:23:47 +0000

    2. Read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- Write a script that creates the database hbtn_0d_2.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Write a script that creates the user user_0d_2.
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
