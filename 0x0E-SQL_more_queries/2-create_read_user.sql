358d55a40cf23e52575719d571e0d9e3df766fc6
Bidicte <malexandra13@yahoo.com>
2023-10-22 16:36:19 +0000

    2. Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,5 +7,4 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
 
---Free memory
-FLUSH PRIVILEGES;
+
