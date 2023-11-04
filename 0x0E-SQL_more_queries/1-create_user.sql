e51e7411bbf9808c990797925941e116544e0d36
Bidicte <malexandra13@yahoo.com>
2023-11-04 02:07:03 +0000

    More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
--- Write a script that creates the MySQL server user user_0d_1.
--- user_0d_1 should have all privileges on your MySQL server
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- creates the MySQL server user user_0d_1 and grant all priviledges
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
